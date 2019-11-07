05baf4364fc3163f5ad7fea2209f45c6d1609b2f
Jonathan Lievin <jonathan.lievin@etudiant.univ-rennes1.fr>
2019-11-07 11:23:50 +0100

    Initiate Android Project

---
+++ app/src/main/java/fr/enssat/pokerplanning/salaun_lievin/MainActivity.kt
@@ -0,0 +1,19 @@
+package fr.enssat.pokerplanning.salaun_lievin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fr.enssat.pokerplanning.salaun_lievin.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/fr/enssat/pokerplanning/salaun_lievin/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package fr.enssat.pokerplanning.salaun_lievin.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
